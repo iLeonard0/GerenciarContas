71381f9c17c4c40c4a76dc7996c310d559480be9
iLeonard0 <folleleonardoprogramming@gmail.com>
2024-10-03 16:48:43 -0300

    Commit inicial

---
+++ app/src/main/java/br/edu/utfpr/trabalhofinal/data/TipoContaEnum.kt
@@ -0,0 +1,6 @@
+package br.edu.utfpr.trabalhofinal.data
+
+enum class TipoContaEnum {
+    RECEITA,
+    DESPESA
+}
---
+++ app/src/main/java/br/edu/utfpr/trabalhofinal/ui/conta/form/FormularioContaScreen.kt
@@ -0,0 +1,450 @@
+package br.edu.utfpr.trabalhofinal.ui.conta.form
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.Notes
+import androidx.compose.material.icons.filled.AttachMoney
+import androidx.compose.material.icons.filled.CalendarMonth
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.*
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import br.edu.utfpr.trabalhofinal.R
+import br.edu.utfpr.trabalhofinal.data.TipoContaEnum
+import br.edu.utfpr.trabalhofinal.ui.theme.TrabalhoFinalTheme
+import br.edu.utfpr.trabalhofinal.ui.utils.composables.Carregando
+import br.edu.utfpr.trabalhofinal.ui.utils.composables.ErroAoCarregar
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+@Composable
+fun FormularioContaScreen(
+    modifier: Modifier = Modifier,
+    onVoltarPressed: () -> Unit,
+    viewModel: FormularioContaViewModel = viewModel(),
+    snackbarHostState: SnackbarHostState = remember { SnackbarHostState() }
+) {
+    LaunchedEffect(viewModel.state.contaPersistidaOuRemovida) {
+        if (viewModel.state.contaPersistidaOuRemovida) {
+            onVoltarPressed()
+        }
+    }
+
+    val context = LocalContext.current
+    LaunchedEffect(snackbarHostState, viewModel.state.codigoMensagem) {
+        viewModel.state.codigoMensagem.takeIf { it > 0 }?.let {
+            snackbarHostState.showSnackbar(context.getString(it))
+            viewModel.onMensagemExibida()
+        }
+    }
+
+    if (viewModel.state.mostrarDialogConfirmacao) {
+        ConfirmationDialog(
+            title = stringResource(R.string.atencao),
+            text = stringResource(R.string.mensagem_confirmacao_remover_contato),
+            onDismiss = viewModel::ocultarDialogConfirmacao,
+            onConfirm = viewModel::removerConta
+        )
+    }
+
+    val contentModifier: Modifier = modifier.fillMaxSize()
+    if (viewModel.state.carregando) {
+        Carregando(modifier = contentModifier)
+    } else if (viewModel.state.erroAoCarregar) {
+        ErroAoCarregar(
+            modifier = contentModifier,
+            onTryAgainPressed = viewModel::carregarConta
+        )
+    } else {
+        Scaffold(
+            modifier = contentModifier,
+            snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
+            topBar = {
+                AppBar(
+                    contaNova = viewModel.state.contaNova,
+                    processando = viewModel.state.salvando || viewModel.state.excluindo,
+                    onVoltarPressed = onVoltarPressed,
+                    onSalvarPressed = viewModel::salvarConta,
+                    onExcluirPressed = viewModel::mostrarDialogConfirmacao
+                )
+            }
+        ) { paddingValues ->
+            FormContent(
+                modifier = Modifier.padding(paddingValues),
+                processando = viewModel.state.salvando || viewModel.state.excluindo,
+                descricao = viewModel.state.descricao,
+                data = viewModel.state.data,
+                valor = viewModel.state.valor,
+                paga = viewModel.state.paga,
+                tipo = viewModel.state.tipo,
+                onDescricaoAlterada = viewModel::onDescricaoAlterada,
+                onDataAlterada = { dataString -> viewModel.onDataAlterada(dataString) },
+                onValorAlterado = viewModel::onValorAlterado,
+                onStatusPagamentoAlterado = { isChecked -> viewModel.onStatusPagamentoAlterado(isChecked.toString()) },
+                onTipoAlterado = viewModel::onTipoAlterado
+            )
+        }
+    }
+}
+
+
+@Composable
+fun ConfirmationDialog(
+    modifier: Modifier = Modifier,
+    title: String? = null,
+    text: String,
+    onDismiss: () -> Unit,
+    onConfirm: () -> Unit,
+    dismissButtonText: String? = null,
+    confirmButtonText: String? = null
+) {
+    AlertDialog(
+        modifier = modifier,
+        title = title?.let { { Text(it) } },
+        text = { Text(text) },
+        onDismissRequest = onDismiss,
+        confirmButton = {
+            TextButton(
+                onClick = onConfirm
+            ) {
+                Text(confirmButtonText ?: stringResource(R.string.confirmar))
+            }
+        },
+        dismissButton = {
+            TextButton(
+                onClick = onDismiss
+            ) {
+                Text(dismissButtonText ?: stringResource(R.string.cancelar))
+            }
+        }
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun AppBar(
+    modifier: Modifier = Modifier,
+    contaNova: Boolean,
+    processando: Boolean,
+    onVoltarPressed: () -> Unit,
+    onSalvarPressed: () -> Unit,
+    onExcluirPressed: () -> Unit
+) {
+    TopAppBar(
+        modifier = modifier.fillMaxWidth(),
+        title = {
+            Text(if (contaNova) {
+                stringResource(R.string.nova_conta)
+            } else {
+                stringResource(R.string.editar_conta)
+            })
+        },
+        navigationIcon = {
+            IconButton(onClick = onVoltarPressed) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = stringResource(R.string.voltar)
+                )
+            }
+        },
+        actions = {
+            if (processando) {
+                CircularProgressIndicator(
+                    modifier = Modifier
+                        .size(60.dp)
+                        .padding(all = 16.dp),
+                    strokeWidth = 2.dp
+                )
+            } else {
+                if (!contaNova) {
+                    IconButton(onClick = onExcluirPressed) {
+                        Icon(
+                            imageVector = Icons.Filled.Delete,
+                            contentDescription = stringResource(R.string.excluir)
+                        )
+                    }
+                }
+                IconButton(onClick = onSalvarPressed) {
+                    Icon(
+                        imageVector = Icons.Filled.Check,
+                        contentDescription = stringResource(R.string.salvar)
+                    )
+                }
+            }
+        },
+        colors = TopAppBarDefaults.topAppBarColors().copy(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            titleContentColor = MaterialTheme.colorScheme.primary,
+            navigationIconContentColor = MaterialTheme.colorScheme.primary,
+            actionIconContentColor = MaterialTheme.colorScheme.primary
+        )
+    )
+}
+
+
+@Preview(showBackground = true)
+@Composable
+private fun AppBarPreview() {
+    TrabalhoFinalTheme {
+        AppBar(
+            contaNova = true,
+            processando = false,
+            onVoltarPressed = {},
+            onSalvarPressed = {},
+            onExcluirPressed = {}
+        )
+    }
+}
+
+@Composable
+fun FormContent(
+    modifier: Modifier = Modifier,
+    processando: Boolean,
+    descricao: CampoFormulario,
+    data: CampoFormulario,
+    valor: CampoFormulario,
+    paga: CampoFormulario,
+    tipo: CampoFormulario,
+    onDescricaoAlterada: (String) -> Unit,
+    onDataAlterada: (String) -> Unit,
+    onValorAlterado: (String) -> Unit,
+    onStatusPagamentoAlterado: (Boolean) -> Unit,
+    onTipoAlterado: (String) -> Unit
+) {
+    Column(
+        modifier = modifier
+            .padding(all = 16.dp)
+            .imePadding()
+            .verticalScroll(rememberScrollState()),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Icon(
+                imageVector = Icons.AutoMirrored.Filled.Notes,
+                contentDescription = stringResource(R.string.descricao),
+                tint = MaterialTheme.colorScheme.outline
+            )
+            FormTextField(
+                modifier = Modifier.fillMaxWidth(),
+                titulo = stringResource(R.string.descricao),
+                campoFormulario = descricao,
+                onValorAlterado = onDescricaoAlterada,
+                enabled = !processando
+            )
+        }
+
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Icon(
+                imageVector = Icons.Filled.AttachMoney,
+                contentDescription = stringResource(R.string.valor),
+                tint = MaterialTheme.colorScheme.outline
+            )
+            FormTextField(
+                modifier = Modifier.fillMaxWidth(),
+                titulo = stringResource(R.string.valor),
+                campoFormulario = valor,
+                onValorAlterado = onValorAlterado,
+                enabled = !processando
+            )
+        }
+
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Icon(
+                imageVector = Icons.Filled.CalendarMonth,
+                contentDescription = stringResource(R.string.data),
+                tint = MaterialTheme.colorScheme.outline
+            )
+            DatePickerField(
+                modifier = Modifier.fillMaxWidth(),
+                titulo = stringResource(R.string.data),
+                selectedDate = data.valor,
+                onDateSelected = onDataAlterada
+            )
+        }
+
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Checkbox(
+                checked = paga.valor == "true",
+                onCheckedChange = { onStatusPagamentoAlterado(it) }
+            )
+            Text(text = stringResource(R.string.paga))
+        }
+
+        Row {
+            RadioButton(
+                selected = tipo.valor == TipoContaEnum.RECEITA.name,
+                onClick = { onTipoAlterado(TipoContaEnum.RECEITA.toString()) }
+            )
+            Text(text = stringResource(R.string.receita))
+
+            Spacer(modifier = Modifier.size(8.dp))
+
+            RadioButton(
+                selected = tipo.valor == TipoContaEnum.DESPESA.name,
+                onClick = { onTipoAlterado(TipoContaEnum.DESPESA.toString()) }
+            )
+            Text(text = stringResource(R.string.despesa))
+        }
+    }
+}
+
+
+@Composable
+fun DatePickerField(
+    modifier: Modifier = Modifier,
+    titulo: String,
+    selectedDate: String,
+    onDateSelected: (String) -> Unit
+) {
+    var showDatePicker by remember { mutableStateOf(false) }
+
+    OutlinedTextField(
+        value = selectedDate,
+        onValueChange = {},
+        modifier = modifier,
+        label = { Text(titulo) },
+        readOnly = true,
+        trailingIcon = {
+            IconButton(onClick = { showDatePicker = true }) {
+                Icon(Icons.Filled.CalendarMonth, contentDescription = "Selecionar Data")
+            }
+        }
+    )
+
+    if (showDatePicker) {
+        DatePickerDialog(
+            initialDate = LocalDate.parse(selectedDate, DateTimeFormatter.ofPattern("dd/MM/yyyy")),
+            onDateSelected = {
+                onDateSelected(it.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")))
+                showDatePicker = false
+            },
+            onDismiss = { showDatePicker = false }
+        )
+    }
+}
+
+
+@Composable
+fun DatePickerDialog(
+    initialDate: LocalDate,
+    onDateSelected: (LocalDate) -> Unit,
+    onDismiss: () -> Unit,
+) {
+    val selectedDate by remember { mutableStateOf(initialDate) }
+    var year by remember { mutableStateOf(selectedDate.year) }
+    var month by remember { mutableStateOf(selectedDate.monthValue) }
+    var day by remember { mutableStateOf(selectedDate.dayOfMonth) }
+
+    AlertDialog(
+        onDismissRequest = onDismiss,
+        title = { Text(stringResource(R.string.selecione_a_data)) },
+        text = {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                Text("Ano: $year")
+                Row {
+                    Button(onClick = { if (year > 1900) year-- }) { Text("-") }
+                    Button(onClick = { year++ }) { Text("+") }
+                }
+
+                Text("Mês: $month")
+                Row {
+                    Button(onClick = { if (month > 1) month-- }) { Text("-") }
+                    Button(onClick = { if (month < 12) month++ }) { Text("+") }
+                }
+
+                Text("Dia: $day")
+                Row {
+                    Button(onClick = { if (day > 1) day-- }) { Text("-") }
+                    Button(onClick = { day++ }) { Text("+") }
+                }
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = {
+                onDateSelected(LocalDate.of(year, month, day))
+                onDismiss()
+            }) {
+                Text(stringResource(R.string.confirmar))
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = onDismiss) {
+                Text(stringResource(R.string.cancelar))
+            }
+        }
+    )
+}
+
+@Composable
+fun FormTextField(
+    modifier: Modifier = Modifier,
+    titulo: String,
+    campoFormulario: CampoFormulario,
+    onValorAlterado: (String) -> Unit,
+    enabled: Boolean = true,
+    keyboardCapitalization: KeyboardCapitalization = KeyboardCapitalization.Sentences,
+    keyboardImeAction: ImeAction = ImeAction.Next,
+    keyboardType: KeyboardType = KeyboardType.Text,
+    visualTransformation: VisualTransformation = VisualTransformation.None
+) {
+    Column(modifier = modifier) {
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = campoFormulario.valor,
+            onValueChange = onValorAlterado,
+            label = { Text(titulo) },
+            maxLines = 1,
+            enabled = enabled,
+            isError = campoFormulario.contemErro,
+            keyboardOptions = KeyboardOptions(
+                capitalization = keyboardCapitalization,
+                imeAction = keyboardImeAction,
+                keyboardType = keyboardType
+            ),
+            visualTransformation = visualTransformation
+        )
+        if (campoFormulario.contemErro) {
+            Text(
+                text = stringResource(campoFormulario.codigoMensagemErro),
+                color = MaterialTheme.colorScheme.error,
+                style = MaterialTheme.typography.labelSmall,
+                modifier = Modifier.padding(top = 8.dp)
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun FormContentPreview() {
+    TrabalhoFinalTheme {
+        FormContent(
+            processando = false,
+            descricao = CampoFormulario(),
+            data = CampoFormulario(),
+            valor = CampoFormulario(),
+            paga = CampoFormulario(),
+            tipo = CampoFormulario(),
+            onDescricaoAlterada = {},
+            onDataAlterada = {},
+            onValorAlterado = {},
+            onStatusPagamentoAlterado = {},
+            onTipoAlterado = {}
+        )
+    }
+}
